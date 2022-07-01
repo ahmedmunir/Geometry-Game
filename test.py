b6a1d341142632385381971a29076d4babe4b890
ahmedmunir <ahmedmunireng@gmail.com>
2022-07-01 23:27:04 +0200

    Add Point class and initial/point in rec function

---
+++ test.py
@@ -0,0 +1,12 @@
+class Point:
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def point_in_rec(self, lowleft, upright):
+        if lowleft[0] < self.x < upright[0] and \
+           lowleft[1] < self.y < upright[1]:
+            return True
+        else:
+            return False
